3ea722b4a285cea2aeed7a64456a2e2b6d3ed58b
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-19 19:35:58 +0200

    meh el meh

--- ALU.vhd
+++ ALU.vhd
@@ -7,7 +7,7 @@ ENTITY ALU is
         n : integer := 32
     );
     PORT (
-        Reg1, Reg2 ,Imm_value: IN std_logic_vector(n-1 downto 0);
+        Reg1, Reg2: IN std_logic_vector(n-1 downto 0);
         ALU_selector : IN std_logic_vector(3 downto 0);
         carry_flag, neg_flag, zero_flag, overflow_flag : OUT std_logic;
         ALUout : OUT std_logic_vector(n-1 downto 0)
@@ -20,27 +20,38 @@ ARCHITECTURE struct OF ALU IS
     CONSTANT zero_vector : std_logic_vector(n-1 downto 0) := (others => '0');
 
 BEGIN
-    Process(Reg1, Reg2, ALU_selector, Imm_value,ALUout_sig)
+    Process(Reg1, Reg2, ALU_selector,ALUout_sig)
     BEGIN
         CASE ALU_selector IS
-            WHEN "0000" =>
-                ALUout_sig <= NOT Reg1;  -- NOT operation
-            WHEN "0001" =>
-                ALUout_sig <= std_logic_vector(to_unsigned(0, ALUout_sig'length) - unsigned(Reg1));  -- NEG operation
-            WHEN "0010" =>
-                ALUout_sig <= std_logic_vector(unsigned(Reg1) + 1);  -- INC operation
-            WHEN "0011" =>
-                ALUout_sig <= std_logic_vector(unsigned(Reg1) - 1);  -- DEC operation
-            WHEN "0100" =>
-                ALUout_sig <= std_logic_vector(unsigned(Reg1) + unsigned(Reg2));  -- ADD operation
-            WHEN "0101" =>
-                ALUout_sig <= std_logic_vector(unsigned(Reg1) - unsigned(Reg2));  -- SUB operation
-            WHEN "0110" =>
-                ALUout_sig <= Reg1 AND Reg2;  -- AND operation
-            WHEN "0111" =>
-                ALUout_sig <= Reg1 XOR Reg2;  -- XOR operation
-            WHEN "1000" =>
-                ALUout_sig <= Reg1 OR Reg2;  -- OR operation
+            WHEN "0000" =>   -- NOT operation
+                ALUout_sig <= NOT Reg1;  
+            WHEN "0001" =>   -- NEG operation
+                ALUout_sig <= std_logic_vector(to_unsigned(0, ALUout_sig'length) - unsigned(Reg1));  
+            WHEN "0010" =>   -- INC operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) + 1);  
+            WHEN "0011" =>   -- DEC operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) - 1); 
+            WHEN "0100" =>   -- ADD operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) + unsigned(Reg2));  
+            WHEN "0101" =>   -- SUB operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) - unsigned(Reg2));  
+            WHEN "0110" =>   -- AND operation
+                ALUout_sig <= Reg1 AND Reg2;  
+            WHEN "0111" =>   -- XOR operation
+                ALUout_sig <= Reg1 XOR Reg2;  
+            WHEN "1000" =>   -- OR operation
+                ALUout_sig <= Reg1 OR Reg2; 
+            WHEN "1001" =>   --CMP Operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) - unsigned(Reg2));
+            WHEN "1010" =>   -- LDD operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) + unsigned(Reg2));
+            WHEN "1011" =>   -- STD operation
+                ALUout_sig <= std_logic_vector(unsigned(Reg1) + unsigned(Reg2)); 
+            WHEN "1100" =>   -- ADDI Operation
+                ALUout_sig <=  std_logic_vector(unsigned(Reg1) + unsigned(Reg2));
+            WHEN "1101" =>   -- SUBI Operation
+                ALUout_sig <=  std_logic_vector(unsigned(Reg1) - unsigned(Reg2));
+
             WHEN OTHERS =>
                 ALUout_sig <= Reg1;  -- Default case (NOP)
         END CASE;
@@ -51,13 +62,20 @@ BEGIN
         ELSE
             zero_flag <= '0';
         END IF;
+        -- Check for negative flag
 	    neg_flag <= ALUout_sig(31);
         -- Check for carry flag
-        IF ALU_selector = "0100" AND (unsigned(Reg1) + unsigned(Reg2)) > to_unsigned(2**n-1, n) THEN
+        IF ALU_selector = "0100" AND ALU_selector/="1001" AND ALU_selector/="1010" AND ALU_selector/="1011" AND (unsigned(Reg1) + unsigned(Reg2)) > to_unsigned(2**n-1, n) THEN
             carry_flag <= '1';
         ELSE
             carry_flag <= '0';
         END IF;
-        ALUout<=ALUout_sig;  -- Assign ALU outputPROCESS
+        -- Check for overflow flag
+        IF ALU_selector = "0100"  AND ALU_selector/="1001" AND ALU_selector/="1010" AND ALU_selector/="1011" AND (Reg1(31) = Reg2(31)) AND (ALUout_sig(31) /= Reg1(31)) THEN
+            overflow_flag <= '1';
+        ELSE
+            overflow_flag <= '0';
+        END IF;
+        ALUout<=ALUout_sig;  -- Assign ALU output
 END PROCESS;
 END ARCHITECTURE struct;
--- Controller.vhd
+++ Controller.vhd
@@ -7,7 +7,7 @@ ENTITY Controller IS
     PORT(
         opcode: IN std_logic_vector(5 DOWNTO 0); 
         ZeroFlag: IN std_logic; -- Branching
-        AluSelector: OUT std_logic_vector(4 DOWNTO 0); -- 3 bits subcode and extra bit
+        AluSelector: OUT std_logic_vector(3 DOWNTO 0); -- 3 bits subcode and extra bit
         Branching: OUT std_logic;
         alusource: OUT std_logic; -- ba4of ba5ud el second operand mn el register or immediate
         MWrite, MRead: OUT std_logic;
@@ -16,31 +16,45 @@ ENTITY Controller IS
         SPPointer: OUT std_logic_vector(1 DOWNTO 0);
         MemWRsrc: OUT std_logic;
         interruptsignal:  out std_logic;
-        rtisignal:  out std_logic
+        rtisignal:  out std_logic;
         FreeProtectStore: OUT std_logic_vector(1 DOWNTO 0)
     );
 END Controller;
 
 ARCHITECTURE Controller_Arch OF Controller IS
 BEGIN
-    RegWrite <= '1' WHEN opcode(5 DOWNTO 4) = "00" or opcode = "000000" or opcode = "000011" or opcode = "000100" or opcode = "001010" or opcode = "110111" 
-               ELSE '0';
-
+    RegWrite <= '1' WHEN (opcode(5 downto 4) = "00" and opcode(3 downto 0) /= "1011") or 
+                        opcode(5 downto 0) = "010001" or 
+                        opcode(5 downto 0) = "010010" or
+                        (opcode(5 downto 4) = "11" and 
+                        (opcode(3 downto 1) /= "000" or 
+                         opcode(3 downto 0) /= "0011" or 
+                         opcode(3 downto 0) /= "0100" or 
+                         opcode(3 downto 0) /= "1001"))
+                ELSE '0';
+                    
 
     WBdatasrc <= "01" WHEN opcode(5 DOWNTO 4) = "01"
                  ELSE "00" WHEN opcode = "110010" -- IN
                  ELSE "10";
 
     AluSelector <= "0000" WHEN opcode = "000000" -- NOT
+                 ELSE "0001" WHEN opcode = "000001" -- NEG
+                 ELSE "0010" WHEN opcode = "000010" -- INC
                  ELSE "0011" WHEN opcode = "000011" -- DEC
-                 ELSE "0100" WHEN opcode = "000100" -- MOV
-                 ELSE "1010" WHEN opcode = "001010" -- OR
-                 ELSE "1011" WHEN opcode = "001011" -- CMP
-                 ELSE "1101" WHEN opcode = "110111" -- LDM
-                 ELSE "0000"; -- Default value
+                 ELSE "0100" WHEN opcode = "000110" -- ADD
+                 ELSE "0101" WHEN opcode = "000111" -- SUB
+                 ELSE "0110" WHEN opcode = "001000" -- AND
+                 ELSE "0111" WHEN opcode = "001001" -- XOR
+                 ELSE "1000" WHEN opcode = "001010" -- OR
+                 ELSE "1001" WHEN opcode = "001011" -- CMP
+                 ELSE "1010" WHEN opcode = "010010" -- LDD
+                 ELSE "1011" WHEN opcode = "010011" -- STD
+                 ELSE "1100" WHEN opcode = "110101" -- ADDI
+                 ELSE "1101" WHEN opcode = "110110" -- SUBI 
+                 ELSE "1110"; -- Default value NOP
 
-    Branching <= '1' WHEN opcode(5 DOWNTO 0) = "100000" -- JMP
-                 ELSE ZeroFlag WHEN opcode = "100001" -- JZ
+    Branching <= '1' WHEN opcode(5 downto 4) = "10" or opcode(5 downto 1) = "11100" -- Branching
                  ELSE '0';
 
     alusource <= '1' WHEN opcode = "110101" or opcode = "110110" or opcode = "110111" -- To include ADDI & LDM
@@ -60,64 +74,3 @@ BEGIN
 
 END Controller_Arch;
 
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
-
-USE IEEE.numeric_std.all;
-
-ENTITY Controller IS
-    PORT(
-        opcode: IN std_logic_vector(5 DOWNTO 0); 
-        ZeroFlag: IN std_logic; -- Branching
-        AluSelector: OUT std_logic_vector(4 DOWNTO 0); -- 3 bits subcode and extra bit
-        Branching: OUT std_logic;
-        alusource: OUT std_logic; -- ba4of ba5ud el second operand mn el register or immediate
-        MWrite, MRead: OUT std_logic;
-        WBdatasrc: OUT std_logic_vector(1 DOWNTO 0);
-        RegWrite: OUT std_logic;
-        SPPointer: OUT std_logic_vector(1 DOWNTO 0);
-        MemWRsrc: OUT std_logic;
-        interruptsignal:  out std_logic;
-        rtisignal:  out std_logic
-        FreeProtectStore: OUT std_logic_vector(1 DOWNTO 0)
-    );
-END Controller;
-
-ARCHITECTURE Controller_Arch OF Controller IS
-BEGIN
-    RegWrite <= '1' WHEN opcode(5 DOWNTO 4) = "00" or opcode = "000000" or opcode = "000011" or opcode = "000100" or opcode = "001010" or opcode = "110111" 
-               ELSE '0';
-
-
-    WBdatasrc <= "01" WHEN opcode(5 DOWNTO 4) = "01"
-                 ELSE "00" WHEN opcode = "110010" -- IN
-                 ELSE "10";
-
-    AluSelector <= "0000" WHEN opcode = "000000" -- NOT
-                 ELSE "0011" WHEN opcode = "000011" -- DEC
-                 ELSE "0100" WHEN opcode = "000100" -- MOV
-                 ELSE "1010" WHEN opcode = "001010" -- OR
-                 ELSE "1011" WHEN opcode = "001011" -- CMP
-                 ELSE "1101" WHEN opcode = "110111" -- LDM
-                 ELSE "0000"; -- Default value
-
-    Branching <= '1' WHEN opcode(5 DOWNTO 0) = "100000" -- JMP
-                 ELSE ZeroFlag WHEN opcode = "100001" -- JZ
-                 ELSE '0';
-
-    alusource <= '1' WHEN opcode = "110101" or opcode = "110110" or opcode = "110111" -- To include ADDI & LDM
-                 ELSE '0';
-
-    MWrite <= '1' WHEN opcode = "011111" or opcode = "010011" or opcode = "100010" -- PUSH STD , CALL
-              ELSE '0';
-
-    MRead <= '1' WHEN opcode = "010001" or opcode = "010010" or opcode = "100100" or opcode = "100011" -- LDM for ناوو
-             ELSE '0';
-
-    SPPointer <= "01" WHEN opcode = "010000" OR opcode = "100111" -- PUSH , CALL
-               ELSE "10" WHEN opcode(5 DOWNTO 2) = "1000" OR opcode = "010110" -- RET, RTI, POP
-               ELSE "00"; -- SP=SP
-
-    MemWRsrc <= opcode(5);
-
-END Controller_Arch;
