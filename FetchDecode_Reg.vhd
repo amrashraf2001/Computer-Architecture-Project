275ad4cdf952f1eb5161606707c8875c3729ded4
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-22 20:28:14 +0200

    fetch

--- FetchDecode_Reg.vhd
+++ FetchDecode_Reg.vhd
@@ -3,24 +3,23 @@ use ieee.std_logic_1164.all;
 
 entity FetchDecode_Reg is
 port (
-    A: IN std_logic_vector(96 downto 0); 
+    A: IN std_logic_vector(80 downto 0); 
     clk,en,rest: in std_logic ; 
-    F: out std_logic_vector(96 downto 0));
+    F: out std_logic_vector(80 downto 0));
 
 end entity FetchDecode_Reg;
 
--- in port -> 32 bit (96 downto 65)
--- instruction -> 16 bit  (64 downto 49)
--- immediate -> ha7ot nafs el instruction tani hna, law galy isImmediate b one 16 bit (48 downto 33)
+-- in port -> 32 bit (80 downto 49)
+-- instruction -> 16 bit  (48 downto 33)
 -- pcPlus -> 32 bit (32 downto 1)
--- isImmediate -> 1 bit (0)
+-- exception -> 1 bit (0)
 
 Architecture FetchDecode_Reg of FetchDecode_Reg is
 begin
 	process (clk,rest)
 	begin
 		if rest = '1' then 
-			F<=(64=>'1',63=>'1',others=>'0'); -- lw 3mlt reset aw ha flush odam adman eni 7atet NOP
+			F<=(48=>'1',47=>'1',others=>'0'); -- lw 3mlt reset aw ha flush odam adman eni 7atet NOP
 		elsif rising_edge(clk) and en='1' then 
 			F<=A;
 		end if;
