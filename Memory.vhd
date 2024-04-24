08c5d1f7d118d2deda0ca3f363b52a5fbfe696e2
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-24 15:18:03 +0200

    m

--- Memory.vhd
+++ Memory.vhd
@@ -7,18 +7,20 @@ entity Memory is
     port (
         clk : in std_logic;
         en,rst : IN std_logic;
-        MemoryWrite : IN std_logic;
+        MemoryWrite1 : IN std_logic;
+        MemoryWrite2 : IN std_logic;
         MemoryRead : IN std_logic;
         MemoryEnable : IN std_logic;
-        MemoryAddress : IN std_logic_vector(n-1 DOWNTO 0);
+        MemoryAddress : IN std_logic_vector(1 DOWNTO 0);
         MemoryWriteData : IN std_logic_vector(n-1 DOWNTO 0);
+        CALL_STD: IN std_logic;
         ALUOut : IN std_logic_vector(n-1 DOWNTO 0);
+        pcPlus: IN std_logic_vector(n-1 DOWNTO 0);
         SecondOperand : IN std_logic_vector(n-1 DOWNTO 0);
         SP: IN std_logic_vector(2 DOWNTO 0);
         FreeProtectedStore : IN std_logic_vector(1 DOWNTO 0);
         MemoryOut : OUT std_logic_vector(n-1 DOWNTO 0);
-        WrongAddress : OUT std_logic
-        
+        WrongAddress : OUT std_logic        
     );
 end entity Memory;
 
@@ -53,7 +55,52 @@ architecture Memory_Arch of Memory is
     END component;
     signal ProtectedFlagsInput : std_logic_vector(n-1 DOWNTO 0);
     signal ProtectedFlagsOutput : std_logic;
-    signal StackPointer : std_logic_vector(n-1 DOWNTO 0);
-    
+    signal StackPointerInput : std_logic_vector(n-1 DOWNTO 0);
+    signal StackPointerOutput: std_logic_vector(n-1 DOWNTO 0);
+    signal DataMemoryOutput: std_logic_vector(n-1 DOWNTO 0);
+    signal WriteAddress1_sig: std_logic_vector(n-1 DOWNTO 0);
+    signal WriteAddress2_sig: std_logic_vector(n-1 DOWNTO 0);
+    signal WriteData1: std_logic_vector(n-1 DOWNTO 0);
 
+    begin
+    Data_Memory1 : Data_Memory
+    PORT MAP (
+        Clk => Clk,
+        Rst => Rst, 
+        WriteEnable1 =>MemoryWrite1 , -- replace with your actual write enable 1 signal
+        WriteEnable2 => MemoryWrite2, -- replace with your actual write enable 2 signal
+        ReadAddress => WriteAddress1_sig, -- replace with your actual read address signal
+        WriteAddress1 => WriteAddress1_sig, -- replace with your actual write address 1 signal
+        WriteAddress2 =>WriteAddress2_sig , -- replace with your actual write address 2 signal
+        ReadData => DataMemoryOutput,
+        WriteData1 => SecondOperand, -- replace with your actual write data 1 signal
+        WriteData2 => SecondOperand, -- replace with your actual write data 2 signal
+        WrongAddress => WrongAddress -- replace with your actual wrong address signal
+    );
+	StackReg1:StackReg 
+    port map (
+        d => StackPointerInput,
+        q => StackPointerOutput,
+        clk => clk,
+        rst => rst,
+        en => MemoryEnable
+    );
+    WriteAddress1_sig <= ALUOut WHEN MemoryAddress ="00"
+    ELSE  StackPointerOutput WHEN MemoryAddress ="01"
+    ELSE  std_logic_vector(unsigned(StackPointerOutput) + 1) WHEN MemoryAddress ="10"
+    ELSE std_logic_vector(unsigned(StackPointerOutput) + 2);
+
+    WriteAddress2_sig <= std_logic_vector(unsigned(StackPointerOutput) - 2);
+    StackPointerInput <= std_logic_vector(unsigned(StackPointerOutput) + 1) WHEN SP="000"
+    ELSE std_logic_vector(unsigned(StackPointerOutput) + 2) WHEN SP = "001"
+    ELSE std_logic_vector(unsigned(StackPointerOutput) - 1) WHEN SP="010"
+    ELSE std_logic_vector(unsigned(StackPointerOutput) - 2) WHEN SP="011"
+    ELSE std_logic_vector(unsigned(StackPointerOutput) - 4) WHEN SP="100"
+    ELSE StackPointerOutput WHEN SP="101"
+    ELSE (others=>'0');
+
+    --TODO: THINK OF MUX INPUTS CALL/STD
+    WriteData1<= SecondOperand;
+
+    MemoryOut <= DataMemoryOutput; 
 end architecture Memory_Arch;
--- Processor.vhd
+++ Processor.vhd
@@ -4,14 +4,9 @@ USE IEEE.numeric_std.all;
 
 ENTITY Processor IS
 PORT(
-    clk : IN STD_LOGIC;
-    reset : IN STD_LOGIC;
-    data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-    data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-    data_out_valid : OUT STD_LOGIC;
-    data_out_ready : IN STD_LOGIC;
-    data_in_valid : IN STD_LOGIC;
-    data_in_ready : OUT STD_LOGIC
+    clk : IN std_logic;
+    branchingAddress: IN std_logic_vector(n-1 downto 0);
+    en,rst,interrupt,branchingSel, exceptionSel, stall : IN std_logic;
 );
 END Processor;
 
@@ -20,11 +15,12 @@ ARCHITECTURE Processor_Arch OF Processor IS
 COMPONENT Fetch IS
     PORT (
         clk : IN std_logic;
-        branchingAddress: IN std_logic_vector(31 downto 0);
-        en,rst,interrupt,branchingSel, exceptionSel, stall : IN std_logic;
-        dataout: OUT std_logic_vector(15 DOWNTO 0);
-        pcPlus: OUT std_logic_vector(31 downto 0);
-        WrongAddress: OUT std_logic
+        --branchingAddress: IN std_logic_vector(31 downto 0);
+        en,rst : IN std_logic;
+        --,interrupt,branchingSel, exceptionSel, stall : IN std_logic;
+        --dataout: OUT std_logic_vector(15 DOWNTO 0);
+        --pcPlus: OUT std_logic_vector(31 downto 0);
+        --WrongAddress: OUT std_logic
     );
 END COMPONENT;
 
@@ -95,6 +91,27 @@ port (
 
 end COMPONENT;
 
+COMPONENT Memory is
+    port (
+        clk : in std_logic;
+        en,rst : IN std_logic;
+        MemoryWrite1 : IN std_logic;
+        MemoryWrite2 : IN std_logic;
+        MemoryRead : IN std_logic;
+        MemoryEnable : IN std_logic;
+        MemoryAddress : IN std_logic_vector(1 DOWNTO 0);
+        MemoryWriteData : IN std_logic_vector(31 DOWNTO 0);
+        CALL_STD: IN std_logic;
+        ALUOut : IN std_logic_vector(31 DOWNTO 0);
+        pcPlus: IN std_logic_vector(31 DOWNTO 0);
+        SecondOperand : IN std_logic_vector(31 DOWNTO 0);
+        SP: IN std_logic_vector(2 DOWNTO 0);
+        FreeProtectedStore : IN std_logic_vector(1 DOWNTO 0);
+        MemoryOut : OUT std_logic_vector(31 DOWNTO 0);
+        WrongAddress : OUT std_logic        
+    );
+end COMPONENT;
+
 -- MEMORY -> WRITEBACK BUFFER
 COMPONENT MemoryWriteBack_Reg is
 port (
@@ -116,5 +133,80 @@ COMPONENT WriteBack is
     );
 end COMPONENT;
 BEGIN
---Fetch1: Fetch PORT MAP(clk,branchingAddress,en,rst,interrupt,branchingSel, exceptionSel, stall,dataout,pcPlus,WrongAddress);
+--FD INPUT SIGNALS
+SIGNAL InPort_FDIN : STD_LOGIC_VECTOR(31 downto 0);
+SIGNAL Instruction_FDIN: STD_LOGIC_VECTOR(15 DOWNTO 0);
+SIGNAL PCPlus_FDIN: STD_LOGIC_VECTOR(31 downto 0);
+SIGNAL WrongAddress_FDIN: STD_LOGIC;
+SIGNAL BranchSel, ExceptionSel, Stall,Interrupt: STD_LOGIC;
+SIGNAL BranchingAddress_FIN: STD_LOGIC_VECTOR(31 downto 0);
+SIGNAL FDIN: STD_LOGIC_VECTOR(80 downto 0);
+--FETCH PORT MAP
+-- 3ayez azabar el branching address ama 23ml ba2i el port maps
+--
+Fetch1:Fetch PORT MAP (
+    clk=>clk,
+    branchingAddress=>BranchingAddress_FIN,
+    en=>en,
+    rst=>rst,
+    interrupt=>Interrupt,
+    branchingSel=>BranchSel,
+    exceptionSel=>ExceptionSel,
+    stall=>Stall,
+    dataout=>Instruction_FDIN,
+    pcPlus=>PCPlus_FDIN,
+    WrongAddress=>WrongAddress_FDIN
+    );
+
+--FD BUFFER PORT MAP
+FDIN<=InPort_FDIN & Instruction_FDIN & PCPlus_FDIN;
+SIGNAL FDOUT: STD_LOGIC_VECTOR(80 downto 0);
+SIGNAL FLUSH: STD_LOGIC;
+FetchDecode_Reg1:FetchDecode_Reg PORT MAP (
+    A=>FDIN,
+    clk=>clk,
+    en=>en,
+    rest=>FLUSH,
+    F=>FDOUT
+    );
+    SIGNAL writeBackEnable_DIN, PredictorEnable_DIN: STD_LOGIC;
+    SIGNAL WritePort1_DIN, WritePort2_DIN: STD_LOGIC_VECTOR(31 downto 0);
+    SIGNAL WriteAdd1_DIN, WriteAdd2_DIN: STD_LOGIC_VECTOR(2 downto 0);
+    SIGNAL SwapedFromWriteBackToDecode: STD_LOGIC;
+    SIGNAL ImmediateValue_DIN: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL ReadData1_DOUT, ReadData2_DOUT: STD_LOGIC_VECTOR(31 downto 0);
+    SIGNAL ALUSelector_DOUT: STD_LOGIC_VECTOR(3 downto 0);
+    SIGNAL Branching_DOUT : STD_LOGIC;
+    SIGNAL ALUSource_DOUT :STD_LOGIC;
+    SIGNAL 
+Decode1: Decode PORT MAP (
+    Clk=>clk,
+    Rst=>rst,
+    writeBackEnable=>writeBackEnable_DIN,
+    PredictorEnable=>PredictorEnable_DIN,
+    Instruction=>FDOUT(48 downto 33),
+    writeport1=>WritePort1_DIN,
+    writeport2=>WritePort2_DIN,
+    WriteAdd1=>WriteAdd1_DIN,
+    WriteAdd2=>WriteAdd2_DIN,
+    Flush=>FLUSH,
+    Swaped=>SwapedFromWriteBackToDecode,
+    ImmediateValue=>ImmediateValue_DIN,
+    ReadData1=>ReadData1_DOUT,
+    ReadData2=>ReadData2_DOUT,
+    AluSelector=>AluSelector,
+    Branching=>Branching,
+    alusource=>AluSource,
+    MWrite=>MWrite,
+    MRead=>MRead,
+    WBdatasrc=>WBdatasrc,
+    RegWrite=>RegWrite,
+    SPPointer=>SPPointer,
+    interruptsignal=>InterruptSignal,
+    pcSource=>PCSource,
+    rtisignal=>RTISignal,
+    FreeProtectStore=>FreeProtectStore,
+    swap=>Swap
+    );
+
 END Processor_Arch;
