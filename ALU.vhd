3cc68084e239914e7e35cc9626393f96ca5456a7
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-25 05:09:45 +0200

    amr is here

--- ALU.vhd
+++ ALU.vhd
@@ -88,7 +88,9 @@ END PROCESS;
         --     else '0' when ALU_selector /= "1010" and ALU_selector /= "1011" AND ALU_selector /= "1111" AND ALU_selector /= "1110" AND (to_integer(signed(ALUout_sig(31 downto 0))) >= 0)
         --     else FlagFeedbackNeg;
         -- Check for carry flag
-        carry_flag <= '1' when ALU_selector = "0011" and extendedA(31 downto 0) = zero_vector -- shuf el general beta3 el borrow
-        else ALUout_sig(32) when ALU_selector = "0100" or ALU_selector = "0101" or ALU_selector = "1100" or ALU_selector = "1101" or ALU_selector = "0010" or ALU_selector = "0011" 
-            else FlagFeedbackCarry;
+        -- Check for carry flag during CMP operation
+        carry_flag <= '1' when (ALU_selector = "1001" or ALU_selector = "1101" or ALU_selector = "0101" or ALU_selector = "0011") and (ALUout_sig(31) = '1')
+        else ALUout_sig(32) when ALU_selector = "0100"  or ALU_selector = "1100"  or ALU_selector = "0010" 
+        else '0' when (ALU_selector = "1001" or ALU_selector = "1101" or ALU_selector = "0101" or ALU_selector = "0011") and (ALUout_sig(31) = '0')
+        else FlagFeedbackCarry;
 END ARCHITECTURE struct;
