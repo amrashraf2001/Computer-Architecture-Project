71d6f99e2c77fc0511065e3c345a1b1dfe4b8bb8
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-29 13:07:37 +0300

    data memory fix

--- Data_Memory.vhd
+++ Data_Memory.vhd
@@ -3,55 +3,45 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 
 ENTITY Data_Memory IS
-GENERIC(n : integer :=32);
-    PORT(
-        Clk, Rst, WriteEnable1, WriteEnable2 : IN STD_LOGIC;
-        ReadAddress, WriteAddress1, WriteAddress2 : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-        ReadData : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-        WriteData1, WriteData2 : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+    GENERIC (n : INTEGER := 32);
+    PORT (
+        Clk, Rst, WriteEnable : IN STD_LOGIC;
+        ReadAddress, WriteAddress : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+        ReadData : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+        WriteData : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
         WrongAddress : OUT STD_LOGIC
     );
 END Data_Memory;
 
 ARCHITECTURE Data_Memory_Architecture OF Data_Memory IS
-    
-	TYPE ram_type IS ARRAY(0 TO 1023) OF std_logic_vector(n-1 DOWNTO 0);
-	SIGNAL ram : ram_type ;
+    TYPE ram_type IS ARRAY (0 TO 1023) OF STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+    SIGNAL ram : ram_type;
 
 BEGIN
-   PROCESS(Clk,Rst,WriteEnable1,WriteEnable2,WriteAddress1,WriteAddress2,ReadAddress) IS
-			BEGIN
-				IF rising_edge(Clk) THEN  
-					IF Rst='1' THEN
-						FOR i IN ram'range LOOP
-							ram(i) <= (others => '0');
-						END LOOP;
-					ELSIF WriteEnable1 = '1' OR WriteEnable2='1' THEN
-                    IF WriteEnable1 = '1' THEN
-                        IF WriteAddress1 < "00000000000000000000010000000000" THEN
-						    ram(to_integer(unsigned(WriteAddress1))) <= WriteData1;
-                            WrongAddress <= '0';
-                        ELSE
-                            WrongAddress <= '1';
-                        END IF;
-					END IF;
-                    IF WriteEnable2 = '1' THEN
-                        IF WriteAddress2 < "00000000000000000000010000000000" THEN
-                            ram(to_integer(unsigned(WriteAddress2))) <= WriteData2;
-                            WrongAddress <= '0';
-                        ELSE
-                            WrongAddress <= '1';
-                        END IF;
-                    END IF;
-                    END IF;
-				END IF;
-                IF ReadAddress < "00000000000000000000010000000000" THEN
-                    ReadData <= ram(to_integer(unsigned(ReadAddress)));
+    PROCESS (Clk, Rst)
+    BEGIN
+        IF Rst = '1' THEN
+            FOR i IN ram'range LOOP
+                ram(i) <= (OTHERS => '0');
+            END LOOP;
+        ELSIF RISING_EDGE(Clk) THEN
+            IF WriteEnable = '1' THEN
+                IF WriteAddress < "00000000000000000000010000000000" THEN
+                    ram(to_integer(unsigned(WriteAddress))) <= WriteData;
                     WrongAddress <= '0';
                 ELSE
                     WrongAddress <= '1';
-                    ReadData <= (others => '0');
+                END IF;
             END IF;
-	END PROCESS;   
+
+            IF ReadAddress < "00000000000000000000010000000000" THEN
+                ReadData <= ram(to_integer(unsigned(ReadAddress)));
+                WrongAddress <= '0';
+            ELSE
+                WrongAddress <= '1';
+                ReadData <= (OTHERS => '0');
+            END IF;
+        END IF;
+    END PROCESS;
 
 END Data_Memory_Architecture;
